51f2f53ab27c77b50de72f834993de5fae303523
karan fauzdar <karanfauzdar442@gmail.com>
2025-02-15 01:18:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Weather from './components/Weather'
+
+const App = () => {
+  return (
+    <div className="w-full min-h-screen bg-gradient-to-b from-teal-400 to-blue-200 sm:h-auto md:h-screen lg:h-screen">
+
+    <Weather/>
+    </div>
+  )
+}
+
+export default App
